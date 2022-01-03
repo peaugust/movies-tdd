45b76b066d549c1c723ecf31204ca72975802122
Pedro <pedro.freddi@jungledevs.com>
2022-01-03 17:38:54 -0300

    Initial Commit

---
+++ Shared/movies_tddApp.swift
@@ -0,0 +1,17 @@
+//
+//  movies_tddApp.swift
+//  Shared
+//
+//  Created by Jungle Devs on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct movies_tddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
